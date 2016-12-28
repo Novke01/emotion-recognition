a591a1abc5adc757dc89ffea54fc1ab96fa30d27
Aleksandar Novakovic <anovakovic01@gmail.com>
2016-12-28 08:51:51 +0100

    CNN model improved.

--- cnn_model/load_trained_model.py
+++ cnn_model/load_trained_model.py
@@ -1,6 +1,8 @@
 from keras.models import load_model
+import numpy as np
 
 from data.data import load_data
+
 
 if __name__ == '__main__':
 
@@ -8,12 +10,38 @@ if __name__ == '__main__':
 
     print 'Data loaded.'
 
-    model = load_model('./cnn_model/trained_model.h5')
+    model = load_model('./cnn_model/trained_deep_model.h5')
 
     print 'Model loaded.'
 
-    score = model.evaluate(test_data, test_labels, verbose=0)
+    # score = model.evaluate(test_data, test_labels, verbose=0)
+    #
+    # print 'Model evaluated.'
+    #
+    # print score
 
-    print 'Model evaluated.'
+    size = 32
 
-    print score
+    result = model.predict_classes(test_data, verbose=0)
+
+    print result
+
+    real_labels = []
+    for l in test_labels:
+        real_labels.append(np.where(l == 1.)[0][0])
+
+    real_labels = np.array(real_labels)
+
+    counts = [0, 0, 0, 0, 0, 0, 0]
+
+    sizes = [0, 0, 0, 0, 0, 0, 0]
+
+    n = len(real_labels)
+
+    for index in xrange(n):
+        if result[index] == real_labels[index]:
+            counts[real_labels[index]] += 1
+        sizes[real_labels[index]] += 1
+
+    for i in xrange(7):
+        print str(counts[i] / float(sizes[i])) + '%'
--- cnn_model/train.py
+++ cnn_model/train.py
@@ -10,25 +10,39 @@ if __name__ == '__main__':
 
     model = Sequential()
 
-    model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(1, 48, 48)))
+    model.add(Convolution2D(64, 3, 3, border_mode='valid', input_shape=(1, 48, 48)))
     model.add(Activation('relu'))
 
-    model.add(Convolution2D(64, 3, 3))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(128, 5, 5))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(512, 3, 3))
     model.add(Activation('relu'))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(128))
+
+    model.add(Dense(256))
     model.add(Activation('relu'))
+
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+
     model.add(Dropout(0.5))
     model.add(Dense(7))
     model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
-    model.fit(train_data, train_labels, batch_size=64, nb_epoch=20, verbose=1,
+    model.fit(train_data, train_labels, batch_size=64, nb_epoch=50, verbose=1,
               validation_data=(test_data, test_labels),
-              callbacks=[ModelCheckpoint('./cnn_model/trained_model.h5', monitor='val_loss', verbose=1, save_best_only=True,
-                                         save_weights_only=False, mode='auto')])
+              callbacks=[ModelCheckpoint('./cnn_model/trained_deep_model.h5', monitor='val_loss', verbose=1,
+                                         save_best_only=True, save_weights_only=False, mode='auto')])
